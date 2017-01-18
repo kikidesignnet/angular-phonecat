16a1e3ce674454b55559205fa4ad824438fc2402
kikibres <kristinabressler@yahoo.com>
2017-01-18 11:55:05 -0500

    Made changes to js files

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -9,14 +9,19 @@ angular.
         this.phones = [
           {
             name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
+            snippet: 'Fast just got faster with Nexus S.',
+            age: 1
           }, {
           name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 2
         }, {
           name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
         }
       ];
+      //set the default value of orderProp to age instead of to uninitialized
+      this.orderProp = 'age';
       }
     });
