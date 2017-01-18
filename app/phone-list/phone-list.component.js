83ddb1493ebecfc97e8fda2a219137a54f98b11e
kikibres <kristinabressler@yahoo.com>
2017-01-18 00:12:37 -0500

    Created new template.html and edited other files

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -3,14 +3,8 @@
 angular.
   module('phoneList').
     component('phoneList', {
-      template:
-        '<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
-        '<ul>' +
-          '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
-            '</li>' +
-            '</ul>',
+      //Note: The URL is relate to our `index.html` file
+      templateUrl: 'phone-list/phone-list.template.html',
       controller: function PhoneListController() {
         this.phones = [
           {
