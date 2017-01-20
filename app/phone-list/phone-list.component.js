c556786c9bdd63a49c96ad1cf481fe63af05a3bd
kikibres <kristinabressler@yahoo.com>
2017-01-20 18:43:29 -0500

    Step 13 Angular JS tutorial

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,15 +5,10 @@ angular.
     component('phoneList', {
       //Note: The URL is relate to our `index.html` file
       templateUrl: 'phone-list/phone-list.template.html',
-      controller: ['$http', function PhoneListController($http) {
-        var self = this;
-        self.orderProp = 'age';
-
-        $http.get('phones/phones.json').then(function(response) {
-          self.phones = response.data;
-        });
-
-      }
-    ]
+      controller: ['Phone', function PhoneListController(Phone) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+        }
+      ]
 
     });
